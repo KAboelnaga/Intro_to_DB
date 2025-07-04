f8e1e0371ba9b852d273fade51e690162fcfcdf9
Kareem Aboelnaga <kaaboelnaga@gmail.com>
2025-07-05 00:21:13 +0300

    Add bookstore schema SQL file

--- MySQLServer.py
+++ MySQLServer.py
@@ -16,7 +16,7 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as err:
+    except mysql.connector.Error as err:
         print(f"Error: {err}")
 
     finally:
